cf0c952fd89e7cf83343898307f59ee7d258d78f
neeleshg <ngurjar@neeleshgurjar.co.in>
2017-08-01 11:08:54 +0530

    Modified variables

--- single-node/variables.tf
+++ single-node/variables.tf
@@ -1,6 +1,11 @@
-variable "cluster_name" {}
-variable "admin_pass"   {}
-variable "region"   {}
-variable "accesskey"   {}
-variable "secretkey"   {}
+variable "cluster_name"		{}
+variable "admin_pass"   	{}
+variable "region"   		{}
+variable "accesskey"   		{}
+variable "secretkey"   		{}
+variable "ami_id" 		{}
+variable "instance_type"	{}
+variable "key_name"		{}
+variable "subnet_id"		{}
+variable "security_group"	{}
 
